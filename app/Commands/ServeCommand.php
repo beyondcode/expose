f54c13aec48041e7e53b6eef1516e61eb1253603
Marcel Pociot <m.pociot@gmail.com>
2020-04-14 21:36:51 +0200

    wip

--- app/Commands/ServeCommand.php
+++ app/Commands/ServeCommand.php
@@ -8,13 +8,14 @@ use LaravelZero\Framework\Commands\Command;
 
 class ServeCommand extends Command
 {
-    protected $signature = 'serve';
+    protected $signature = 'serve {host=127.0.0.1}';
 
     protected $description = 'Start the shaft server';
 
     public function handle()
     {
         (new Factory())
+            ->setHost($this->argument('host'))
             ->createServer()
             ->run();
     }
