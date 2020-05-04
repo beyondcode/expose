120e9cbd75dc1759890454dd1dec7a6c79f772fe
Marcel Pociot <m.pociot@gmail.com>
2020-05-04 21:30:21 +0200

    wip

--- app/Commands/ShareCurrentWorkingDirectoryCommand.php
+++ app/Commands/ShareCurrentWorkingDirectoryCommand.php
@@ -10,10 +10,22 @@ class ShareCurrentWorkingDirectoryCommand extends ShareCommand
 
     public function handle()
     {
-        $this->input->setArgument('host', basename(getcwd()).'.test');
+        $this->input->setArgument('host', basename(getcwd()).'.'.$this->detectTld());
 
         $this->input->setOption('subdomain', basename(getcwd()));
 
         parent::handle();
+    }
+
+    protected function detectTld(): string
+    {
+        $valetConfigFile = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . '.config' . DIRECTORY_SEPARATOR . 'valet' . DIRECTORY_SEPARATOR . 'config.json';
+
+        if (file_exists($valetConfigFile)) {
+            $valetConfig = json_decode(file_get_contents($valetConfigFile));
+            return $valetConfig->tld;
+        }
+
+        return 'test';
     }
 }
