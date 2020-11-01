7459c0189bdfd515641e502735f7652d8e1ff6bc
Marcel Pociot <mpociot@users.noreply.github.com>
2020-11-01 16:47:29 +0000

    Apply fixes from StyleCI

--- app/Server/Http/Controllers/TunnelMessageController.php
+++ app/Server/Http/Controllers/TunnelMessageController.php
@@ -75,7 +75,7 @@ class TunnelMessageController extends Controller
 
         $httpConnection = $this->connectionManager->storeHttpConnection($httpConnection, $requestId);
 
-        transform($this->passRequestThroughModifiers($request, $httpConnection), function (Request $request) use ($controlConnection , $requestId) {
+        transform($this->passRequestThroughModifiers($request, $httpConnection), function (Request $request) use ($controlConnection, $requestId) {
             $controlConnection->once('proxy_ready_'.$requestId, function (ConnectionInterface $proxy) use ($request) {
                 // Convert the Laravel request into a PSR7 request
                 $psr17Factory = new Psr17Factory();
