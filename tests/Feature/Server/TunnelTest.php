2934731c7a2720ee6b303331ac23b6e90b69e896
Marcel Pociot <m.pociot@gmail.com>
2021-01-14 13:20:59 +0100

    Add PHP8 compatibility to v1.x (#177)
    * Add PHP8 compatible requirements
    * Readd DNS to allow resolving local shared domains

--- tests/Feature/Server/TunnelTest.php
+++ tests/Feature/Server/TunnelTest.php
@@ -135,7 +135,7 @@ class TunnelTest extends TestCase
 
     protected function createTestHttpServer()
     {
-        $server = new Server(function (ServerRequestInterface $request) {
+        $server = new Server($this->loop, function (ServerRequestInterface $request) {
             return new Response(200, ['Content-Type' => 'text/plain'], 'Hello World!');
         });
 
