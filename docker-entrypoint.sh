9e7de395a09194a29d8c058bd5d5613058975c97
Thomas Hondema <thomas.hondema@multisafepay.com>
2021-06-23 11:31:12 +0200

    Improved support for the share command in docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/src/expose token ${token} && sed -i -E "s|'dns'\\s?=>\\s?'.*'|'dns' => true|g" ${exposeConfigPath}
-
 sed -i "s|username|${username}|g" ${exposeConfigPath} && sed -i "s|password|${password}|g" ${exposeConfigPath}
+
+sed -i -E "s|'dns'\\s?=>\\s?'.*'|'dns' => true|g" ${exposeConfigPath}
+
+/src/expose token ${token}
 
 if [[ $# -eq 0 ]]; then
     exec /src/expose serve ${domain} --port ${port} --validateAuthTokens
