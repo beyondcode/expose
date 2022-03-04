7fadb687cc90bfe09fb750d0504be51810374d1a
Marcel Pociot <m.pociot@gmail.com>
2022-03-04 12:21:44 +0100

    Allow specifying local config files when starting the server

--- app/Commands/ServeCommand.php
+++ app/Commands/ServeCommand.php
@@ -3,19 +3,34 @@
 namespace App\Commands;
 
 use App\Server\Factory;
+use InvalidArgumentException;
 use LaravelZero\Framework\Commands\Command;
 use React\EventLoop\LoopInterface;
 
 class ServeCommand extends Command
 {
-    protected $signature = 'serve {hostname=localhost} {host=0.0.0.0}  {--validateAuthTokens} {--port=8080}';
+    protected $signature = 'serve {hostname=localhost} {host=0.0.0.0}  {--validateAuthTokens} {--port=8080} {--config=}';
 
     protected $description = 'Start the expose server';
+
+    protected function loadConfiguration(string $configFile)
+    {
+        $configFile = realpath($configFile);
+
+        throw_if(! file_exists($configFile), new InvalidArgumentException("Invalid config file {$configFile}"));
+
+        $localConfig = require $configFile;
+        config()->set('expose', $localConfig);
+    }
 
     public function handle()
     {
         /** @var LoopInterface $loop */
         $loop = app(LoopInterface::class);
+
+        if ($this->option('config')) {
+            $this->loadConfiguration($this->option('config'));
+        }
 
         $loop->futureTick(function () {
             $this->info('Expose server running on port '.$this->option('port').'.');
