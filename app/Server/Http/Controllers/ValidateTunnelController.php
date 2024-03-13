37e44c0f478bc2996abb8a72691bff41fe76f570
Siebe Vanden Eynden <siebe.vandeneynden@team.blue>
2024-03-13 13:37:39 +0100

    Add key and proper request for not tunnel domains

--- app/Server/Http/Controllers/ValidateTunnelController.php
+++ app/Server/Http/Controllers/ValidateTunnelController.php
@@ -4,33 +4,65 @@ namespace App\Server\Http\Controllers;
 
 use App\Contracts\ConnectionManager;
 use App\Http\Controllers\Controller;
+use App\Server\Configuration;
 use App\Server\Connections\ControlConnection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Ratchet\ConnectionInterface;
 
 class ValidateTunnelController extends Controller
 {
     private ConnectionManager $connectionManager;
+    private Configuration $configuration;
 
-
-    public function __construct(ConnectionManager $connectionManager)
-    {
+    public function __construct(
+        Configuration $configuration,
+        ConnectionManager $connectionManager,
+    ) {
         $this->connectionManager = $connectionManager;
+        $this->configuration = $configuration;
     }
 
     public function handle(Request $request, ConnectionInterface $httpConnection)
     {
+        $key = $request->get('key');
+
+        // Only allow requests with the correct key
+        if ($key !== $this->getAuthorizedKey()) {
+            $httpConnection->send(
+                respond_json(['exists' => false], 401),
+            );
+            $httpConnection->close();
+
+            return;
+        }
+
         $domain = $request->get('domain');
         if ($domain === null) {
             $httpConnection->send(
                 respond_json(['exists' => false, 'error' => 'invalid_domain'], 404),
             );
+            $httpConnection->close();
 
             return;
         }
 
-        /** @var Collection $sites */
+        // If the domain is the same as the hostname, then it requested the main domain
+        $hostname = $this->configuration->hostname();
+        if ($hostname === $domain) {
+            $this->isSuccessful($httpConnection);
+
+            return;
+        }
+
+        // Also allow the admin dashboard
+        $adminSubdomain = config('expose.admin.subdomain');
+        if ($domain === $adminSubdomain . '.' . $hostname) {
+            $this->isSuccessful($httpConnection);
+
+            return;
+        }
+
+        // Check if the domain is a tunnel
         $sites = collect($this->connectionManager->getConnections())
             ->filter(function ($site) use ($domain) {
                 $isControlConnection = get_class($site) === ControlConnection::class;
@@ -38,12 +70,12 @@ class ValidateTunnelController extends Controller
                     return false;
                 }
 
-
                 $fqdn = sprintf(
                     '%s.%s',
                     $site->subdomain,
                     $site->serverHost,
                 );
+
                 return $fqdn === $domain;
             })
             ->map(function (ControlConnection $site) {
@@ -54,10 +86,25 @@ class ValidateTunnelController extends Controller
                 );
             });
 
-        $response = $sites->count() === 0
-            ? respond_json(['exists' => false, 'error' => 'no_tunnel_found'], 404)
-            : respond_json(['exists' => true, 'sites' => $sites->toArray()]);
+        if ($sites->count() > 0) {
+            $this->isSuccessful($httpConnection);
 
-        $httpConnection->send($response);
+            return;
+        }
+
+
+        $httpConnection->send(respond_json(['exists' => false, 'error' => 'no_tunnel_found'], 404));
+        $httpConnection->close();
+    }
+
+    private function isSuccessful(ConnectionInterface $connection): void
+    {
+        $connection->send(respond_json(['exists' => true]));
+        $connection->close();
+    }
+
+    private function getAuthorizedKey(): string
+    {
+        return config('expose.validate_tunnel.authorized_key');
     }
 }
