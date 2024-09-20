0faf9c18a92059e4e5fb45ce50033d218ebf410c
Di <scharf.diana@googlemail.com>
2024-09-20 15:56:27 +0200

    Use proxy for development

--- resources/js/internal-dashboard/vite.config.ts
+++ resources/js/internal-dashboard/vite.config.ts
@@ -7,6 +7,14 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        proxy: {
+          '/api': {
+            target: 'http://127.0.0.1:4040',
+            changeOrigin: true,
+          },
+        },
+      },
     css: {
         postcss: {
             plugins: [tailwind(), autoprefixer()],
