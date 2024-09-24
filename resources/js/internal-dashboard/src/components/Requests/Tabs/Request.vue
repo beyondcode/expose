d8ebb7fc8de17418227c313285658038e2c823d8
Di <scharf.diana@googlemail.com>
2024-09-24 10:23:00 +0200

    Query parameters

--- resources/js/internal-dashboard/src/components/Requests/Tabs/Request.vue
+++ resources/js/internal-dashboard/src/components/Requests/Tabs/Request.vue
@@ -38,19 +38,40 @@ const requestIsJson = () => {
 
 <template>
     <div class="max-w-full">
+        <div v-if="Object.keys(request.query).length > 0" class="mb-4">
+            <div class="pt-4 font-medium text-base mb-2">Query parameters</div>
+            <div class="flex justify-end">
+                <Button @click="copyToClipboard(toPhpArray(request.query, 'queryParameters'))" variant="outline">
+                    <Icon icon="radix-icons:clipboard-copy" class="h-4 w-4 mr-2" />
+                    Copy as PHP array
+                </Button>
+            </div>
+            <Table class="max-w-full">
+                <TableBody>
+                    <TableRow v-for="[key, value] of Object.entries(request.query)" :key="key">
+                        <TableCell class="w-2/5">
+                            {{ key }}
+                        </TableCell>
+
+                        <TableCell class="w-3/5 break-all">
+                            {{ value }}
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
         <Accordion type="single" collapsible default-value="item-1">
             <AccordionItem value="item-1">
                 <AccordionTrigger>
                     <div class="flex relative z-10 justify-between items-center w-full pr-4">
                         Headers
-
                     </div>
                 </AccordionTrigger>
                 <AccordionContent>
                     <div class="flex justify-end">
                         <Button @click="copyToClipboard(toPhpArray(request.headers, 'headers'))" variant="outline">
                             <Icon icon="radix-icons:clipboard-copy" class="h-4 w-4 mr-2" />
-                            Copy to PHP array
+                            Copy as PHP array
                         </Button>
                     </div>
                     <Table class="max-w-full">
